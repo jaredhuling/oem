304cba2aa11d05e47ec3cdbfaab39e3845bf21c0
jaredhuling <huling@wisc.edu>
2016-03-28 10:02:39 -0500

    worked on base class structure for oem algorithm

---
+++ src/oem_base.h
@@ -0,0 +1,104 @@
+#ifndef OEMBASE_H
+#define OEMBASE_H
+
+#include <RcppEigen.h>
+#include "utils.h"
+
+
+template<typename VecTypeBeta, typename MatTypeX>
+class oemBase
+{
+protected:
+    
+    const int nvars;         // dimension of beta
+    const int nobs;          // number of rows
+    
+    MatTypeX X;              // design matrix
+    MatTypeX A;              // A matrix
+    VectorXd u;              // u
+    VecTypeBeta beta;        // parameters to be optimized
+    VecTypeBeta beta_prev;   // parameters from previous iteration
+    
+    double tol;              // tolerance for convergence
+    
+    virtual void next_beta(VecTypeBeta &res) = 0;
+    
+    virtual void next_u(VectorXd &res) = 0;
+    
+    virtual bool converged()
+    {
+        return (stopRule(beta, beta_prev, tol));
+    }
+    
+    
+    void print_row(int iter)
+    {
+        const char sep = ' ';
+        
+        Rcpp::Rcout << std::left << std::setw(7)  << std::setfill(sep) << iter;
+        Rcpp::Rcout << std::endl;
+    }
+    void print_footer()
+    {
+        const int width = 80;
+        Rcpp::Rcout << std::string(width, '=') << std::endl << std::endl;
+    }
+    
+public:
+    oemBase(int n_, 
+            int p_,
+            double tol_ = 1e-6) :
+    nvars(p_), 
+    nobs(n_),
+    beta(p_),            // allocate space but do not set values
+    beta_prev(p_),       // allocate space but do not set values
+    tol(tol_)
+    {}
+    
+    virtual ~oemBase() {}
+    
+    void update_u()
+    {
+        //VecTypeBeta newbeta(nvars);
+        next_u(u);
+        //beta.swap(newbeta);
+    }
+    
+    void update_beta()
+    {
+        //VecTypeBeta newbeta(nvars);
+        next_beta(beta);
+        //beta.swap(newbeta);
+    }
+    
+    int solve(int maxit)
+    {
+        int i;
+        
+        for(i = 0; i < maxit; ++i)
+        {
+            beta_prev = beta;
+            // old_y = dual_y;
+            //std::copy(dual_y.data(), dual_y.data() + dim_dual, old_y.data());
+            
+            update_u();
+            update_beta();
+            
+            // print_row(i);
+            
+            if(converged())
+                break;
+            
+        }
+        
+        // print_footer();
+        
+        return i + 1;
+    }
+    
+    virtual VecTypeBeta get_beta() { return beta; }
+};
+
+
+
+#endif // OEMBASE_H
