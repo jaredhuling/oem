85df3a146fe4cbaceb55f705b35471f840f3f074
jaredhuling <huling@wisc.edu>
2016-05-24 20:00:02 -0500

    removed printing

--- src/oem_xval_dense.h
+++ src/oem_xval_dense.h
@@ -557,8 +557,6 @@ protected:
         XX /= nobs;
         XY /= nobs;
         
-        std::cout << "xtx(5,5)" << XX.topLeftCorner(5,5) << std::endl;
-        std::cout << "xty(5)" << XY.head(5).adjoint() << std::endl;
         
         Spectra::DenseSymMatProd<double> op(XX);
         Spectra::SymEigsSolver< double, Spectra::LARGEST_ALGE, Spectra::DenseSymMatProd<double> > eigs(&op, 1, 4);
