be33d55f81a2ae71b15e927d2fb9af418aea1085
jaredhuling <huling@wisc.edu>
2017-02-21 00:01:28 -0600

    init.c

--- src/oem_construction.h
+++ src/oem_construction.h
@@ -144,22 +144,4 @@ RcppExport SEXP oem_xval_dense(SEXP,
                                SEXP);
 
 
-static const
-    R_CallMethodDef callMethods[] = {
-        {"Csetattrib", (DL_FUNC) &oemfit, -1},
-        {"Cbmerge", (DL_FUNC) &oem_fit_big, -1},
-        {"Cbmerge", (DL_FUNC) &oem_fit_dense, -1},
-        {"Cbmerge", (DL_FUNC) &oem_fit_logistic_dense, -1},
-        {"Cbmerge", (DL_FUNC) &oem_fit_logistic_sparse, -1},
-        {"Cbmerge", (DL_FUNC) &oem_fit_sparse, -1},
-        {"Cbmerge", (DL_FUNC) &oem_xtx, -1},
-        {"Cbmerge", (DL_FUNC) &oem_xval_dense, -1},
-        {NULL, NULL, 0}
-    };
 
-void attribute_visible R_init_oem(DllInfo *info)
-    // relies on pkg/src/Makevars to mv data.table.so to datatable.so
-{
-    R_registerRoutines(info, NULL, callMethods, NULL, NULL);
-    R_useDynamicSymbols(info, FALSE);
-}
---
+++ src/oem_init.c
@@ -0,0 +1,170 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+/*
+SEXP oemfit(SEXP,
+            SEXP,
+            SEXP,
+            SEXP,
+            SEXP,
+            SEXP,
+            SEXP,
+            SEXP,
+            SEXP,
+            SEXP);
+
+SEXP oem_fit_big(SEXP, 
+                 SEXP, 
+                 SEXP,
+                 SEXP,
+                 SEXP,
+                 SEXP,
+                 SEXP,
+                 SEXP,
+                 SEXP,
+                 SEXP, 
+                 SEXP,
+                 SEXP,
+                 SEXP,
+                 SEXP,
+                 SEXP, 
+                 SEXP,
+                 SEXP,
+                 SEXP);
+
+SEXP oem_fit_dense(SEXP, 
+                   SEXP, 
+                   SEXP,
+                   SEXP,
+                   SEXP,
+                   SEXP,
+                   SEXP,
+                   SEXP,
+                   SEXP,
+                   SEXP, 
+                   SEXP,
+                   SEXP,
+                   SEXP,
+                   SEXP,
+                   SEXP, 
+                   SEXP,
+                   SEXP,
+                   SEXP);
+
+SEXP oem_fit_logistic_dense(SEXP, 
+                            SEXP, 
+                            SEXP,
+                            SEXP,
+                            SEXP,
+                            SEXP,
+                            SEXP,
+                            SEXP,
+                            SEXP,
+                            SEXP, 
+                            SEXP,
+                            SEXP,
+                            SEXP,
+                            SEXP,
+                            SEXP, 
+                            SEXP,
+                            SEXP,
+                            SEXP);
+
+SEXP oem_fit_logistic_sparse(SEXP, 
+                             SEXP, 
+                             SEXP,
+                             SEXP,
+                             SEXP,
+                             SEXP,
+                             SEXP,
+                             SEXP,
+                             SEXP,
+                             SEXP, 
+                             SEXP,
+                             SEXP,
+                             SEXP,
+                             SEXP,
+                             SEXP, 
+                             SEXP,
+                             SEXP,
+                             SEXP);
+
+SEXP oem_fit_sparse(SEXP, 
+                    SEXP, 
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP, 
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP, 
+                    SEXP,
+                    SEXP,
+                    SEXP);
+
+SEXP oem_xtx(SEXP, 
+             SEXP, 
+             SEXP,
+             SEXP,
+             SEXP,
+             SEXP,
+             SEXP,
+             SEXP,
+             SEXP, 
+             SEXP,
+             SEXP,
+             SEXP,
+             SEXP,
+             SEXP,
+             SEXP);
+
+SEXP oem_xval_dense(SEXP, 
+                    SEXP, 
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP, 
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP, 
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP,
+                    SEXP);
+
+
+static const
+R_CallMethodDef callMethods[] = {
+    {"Csetattrib", (DL_FUNC) &oemfit, -1},
+    {"Cbmerge", (DL_FUNC) &oem_fit_big, -1},
+    {"Cbmerge", (DL_FUNC) &oem_fit_dense, -1},
+    {"Cbmerge", (DL_FUNC) &oem_fit_logistic_dense, -1},
+    {"Cbmerge", (DL_FUNC) &oem_fit_logistic_sparse, -1},
+    {"Cbmerge", (DL_FUNC) &oem_fit_sparse, -1},
+    {"Cbmerge", (DL_FUNC) &oem_xtx, -1},
+    {"Cbmerge", (DL_FUNC) &oem_xval_dense, -1},
+    {NULL, NULL, 0}
+};
+*/
+
+
+void R_init_oem(DllInfo *info)
+{
+    R_registerRoutines(info, NULL, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, TRUE);
+}
